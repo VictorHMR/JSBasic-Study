12b849b14734b92ad8c9bb1bcdfaba30bb20e77e
Victor Hugo Machado Rios <75526228+VictorHMR@users.noreply.github.com>
2022-03-01 12:11:49 -0300

    Pokedex 2.5
    Foi adicionado a possibilidade de pesquisar Pokémon's por número ou nome, além de varias mudanças visuais

--- PokeDex/js/dex.js
+++ PokeDex/js/dex.js
@@ -3,8 +3,8 @@
  var NFinal;
  var regiao;
  var GNumber;
- var url= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
-
+ const url= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
+var urlM = url;
 function fetchPokemon(geracao){
     const getPokemonUrl =  id =>`https://pokeapi.co/api/v2/pokemon/${id}`;
     const pokePromises = [];
@@ -47,7 +47,7 @@ switch(geracao){
     break;
     default: Ninicial = 1;
              NFinal = 809;
-             regiao= "Pokedex(1-5gn)";
+             regiao= "Pokedex(1-7gn)";
              GNumber=0;
     break;
     
@@ -59,12 +59,18 @@ switch(geracao){
     .then(pokemons =>{
         const liPoke = pokemons.reduce((accumulator, pokemon) =>{
             const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+            
             accumulator += `
-                <li class="card ${types[0]}">
-                <img class="card-image" alt="${pokemon.name}" width="50%" src="${url}/${pokemon.id}.png" />
+                <li class="card ${types[0]}" onclick="//changePoke('${pokemon.name}')">
+                <img class="card-image" alt="${pokemon.name}" width="50%" src="${urlM}/${pokemon.id}.png" />
                 <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-                <p class="card-subtitle ">${types.join(' | ')}</p> 
-                </li>`;
+                <p class="card-subtitle"> ${types.join(' | ')}</p>
+                <p class="card-subtitle">`  
+                for(i=0; i < types.length;i++){
+                    accumulator += `<img class="iconT" src="img/elements/${types[i]}.ico">`
+                } 
+            accumulator += `</p></li>`
+               
 
             return accumulator
         }, '')
@@ -78,13 +84,66 @@ switch(geracao){
 
 function ShinyChange(){
     if( $("#ShinyC").is(':checked') == true ){
-        url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny"
+        urlM = url + "/shiny"
 
     }else if( $("#ShinyC").is(':checked') == false){
-        url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon"
+        urlM = url;
     }
-    fetchPokemon(GNumber)
+    if($("#search").val() == ""){
+        fetchPokemon(GNumber)
+    }else{
+        searchPoke();
+    }
 }
+
+function searchPoke(){
+    let value = $("#search").val().toLowerCase();
+    if(value == ""){
+        fetchPokemon(GNumber);
+    }
+    else{
+    const getPokemonUrl =  id =>`https://pokeapi.co/api/v2/pokemon/${id}`;
+    const pokePromises = [];
+    pokePromises.push(fetch(getPokemonUrl(value)).then(response => response.json()));
+    Promise.all(pokePromises)
+    .then(pokemons =>{
+        const liPoke = pokemons.reduce((accumulator, pokemon) =>{
+            const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+            pokeP = pokemon.id+"."+ pokemon.name
+            accumulator += `
+                <li class="card ${types[0]} solo" onclick="//changePoke('${pokemon.name}')">
+                <img class="card-image" alt="${pokemon.name}" width="50%" src="${url}/${pokemon.id}.png" />
+                <p class="card-subtitle"> ${types.join(' | ')}</p>
+                <p class="card-subtitle">`  
+                for(i=0; i < types.length;i++){
+                    accumulator += `<img class="iconT" src="img/elements/${types[i]}.ico">`
+                } 
+            accumulator += `</p></li>`
+
+            return accumulator
+        }, '')
+        const ul = document.querySelector('[data-js="pokedex"]');
+        $(ul).html(liPoke);
+        $("#tituloP").html(pokeP);
+
+    })
+    }
+}
+
+function changePoke(poke){
+    $("#search").val(poke);
+    searchPoke();
+}
+
+$("#search").on('keyup', function (event) {
+    if (event.keyCode !== 13) return;
+    searchPoke();
+});
+
+$(".btnG").on('click', function(){
+    $('html, body').animate({scrollTop:0}, 'fast');
+});
+
 fetchPokemon();
 
 
