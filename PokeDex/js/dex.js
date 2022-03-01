6e4ea01471f022e046067d89de6f58d1c7a40048
Victor Hugo Machado Rios <75526228+VictorHMR@users.noreply.github.com>
2022-03-01 15:16:34 -0300

    Pokedex 2.6
    Agora é Possível pesquisar o inicio do nome de um Pokémon e obter resultados !, também tiveram alguns ajustes de front-end e um contador

--- PokeDex/js/dex.js
+++ PokeDex/js/dex.js
@@ -3,8 +3,13 @@
  var NFinal;
  var regiao;
  var GNumber;
- const url= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
+ let pokeP;
+ var contador = 0;
+const url= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
 var urlM = url;
+
+
+
 function fetchPokemon(geracao){
     const getPokemonUrl =  id =>`https://pokeapi.co/api/v2/pokemon/${id}`;
     const pokePromises = [];
@@ -77,6 +82,8 @@ switch(geracao){
         const ul = document.querySelector('[data-js="pokedex"]');
         $(ul).html(liPoke);
         $("#tituloP").html(regiao);
+        $("#aviso").html(NFinal - Ninicial +1 + " Resultados");
+        
     })
 
 
@@ -104,27 +111,62 @@ function searchPoke(){
     else{
     const getPokemonUrl =  id =>`https://pokeapi.co/api/v2/pokemon/${id}`;
     const pokePromises = [];
-    pokePromises.push(fetch(getPokemonUrl(value)).then(response => response.json()));
+
+    if(isNaN(value)){
+        for(let i =Ninicial; i <= NFinal; i++){
+            pokePromises.push(fetch(getPokemonUrl(i)).then(response => response.json()));
+        }
+    }else{
+        pokePromises.push(fetch(getPokemonUrl(value)).then(response => response.json()));
+    }
     Promise.all(pokePromises)
     .then(pokemons =>{
         const liPoke = pokemons.reduce((accumulator, pokemon) =>{
             const types = pokemon.types.map(typeInfo => typeInfo.type.name)
-            pokeP = pokemon.id+"."+ pokemon.name
-            accumulator += `
-                <li class="card ${types[0]} solo" onclick="//changePoke('${pokemon.name}')">
-                <img class="card-image" alt="${pokemon.name}" width="50%" src="${urlM}/${pokemon.id}.png" />
-                <p class="card-subtitle"> ${types.join(' | ')}</p>
-                <p class="card-subtitle">`  
-                for(i=0; i < types.length;i++){
-                    accumulator += `<img class="iconT" src="img/elements/${types[i]}.ico">`
-                } 
-            accumulator += `</p></li>`
+
+            if(isNaN(value)){
+                if(pokemon.name.startsWith(value)){
+                    contador++;
+                    pokeP = value;
+                    accumulator += `
+                        <li class="card ${types[0]}" onclick="//changePoke('${pokemon.name}')">
+                        <img class="card-image" alt="${pokemon.name}" width="50%" src="${urlM}/${pokemon.id}.png" />
+                        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                        <p class="card-subtitle"> ${types.join(' | ')}</p>
+                        <p class="card-subtitle">`  
+                        for(i=0; i < types.length;i++){
+                            accumulator += `<img class="iconT" src="img/elements/${types[i]}.ico">`
+                        } 
+                    accumulator += `</p></li>`
+                    $("#aviso").html(contador + " Resultados");
+                }else{
+                    pokeP = value
+                    $("#aviso").html("Sem Resultados");
+                }
+            }else{
+                pokeP = value;
+                contador = 1;
+                accumulator += `
+                    <li class="card ${types[0]} solo" onclick="//changePoke('${pokemon.name}')">
+                    <img class="card-image" alt="${pokemon.name}" width="50%" src="${urlM}/${pokemon.id}.png" />
+                    <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                    <p class="card-subtitle"> ${types.join(' | ')}</p>
+                    <p class="card-subtitle">`  
+                    for(i=0; i < types.length;i++){
+                        accumulator += `<img class="iconT" src="img/elements/${types[i]}.ico">`
+                    } 
+                accumulator += `</p></li>`
+                $("#aviso").html(contador + " Resultados");
+
+            }
+           
 
             return accumulator
         }, '')
         const ul = document.querySelector('[data-js="pokedex"]');
         $(ul).html(liPoke);
         $("#tituloP").html(pokeP);
+        contador= 0;
 
     })
     }
@@ -137,12 +179,15 @@ function changePoke(poke){
 
 $("#search").on('keyup', function (event) {
     if (event.keyCode !== 13) return;
-    searchPoke();
+    searchPoke()
+    $("#aviso").val("")
+    $("#tituloP").val("")
+    contador = 0;
 });
 
 $(".btnG").on('click', function(){
     $('html, body').animate({scrollTop:0}, 'fast');
-     $("#search").val('');
+    $("#search").val('');
 });
 
 fetchPokemon();
