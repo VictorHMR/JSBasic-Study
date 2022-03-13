9546c2a8c6652045b6a3ef0131a9b1c5d74bcf6f
Victor Hugo Machado Rios <75526228+VictorHMR@users.noreply.github.com>
2022-03-13 19:54:30 -0300

    Add files via upload

--- SiteDeUtilidades/js/conversor.js
+++ SiteDeUtilidades/js/conversor.js
@@ -2,7 +2,7 @@ var Dec;
 var Bin;
 var Hex;
 var Oct;
-
+var Asc;
 $(".RadioButton").on("click", function (event) {
   event = event.target.id;
   if (event == "dec") {
@@ -10,64 +10,91 @@ $(".RadioButton").on("click", function (event) {
     $("#bin").prop("checked", false);
     $("#hex").prop("checked", false);
     $("#oct").prop("checked", false);
+    $("#asc").prop("checked", false);
     $("#numDec").prop("disabled", false);
     $("#numBin").prop("disabled", true);
     $("#numHex").prop("disabled", true);
     $("#numOct").prop("disabled", true);
-    $("#numBin").val('');
-    $("#numHex").val('');
-    $("#numOct").val('');
-
+    $("#numAsc").prop("disabled", true);
+    $("#numBin").val("");
+    $("#numHex").val("");
+    $("#numOct").val("");
+    $("#numAsc").val("");
   }
   if (event == "bin") {
     $("#bin").prop("checked", true);
     $("#dec").prop("checked", false);
     $("#hex").prop("checked", false);
     $("#oct").prop("checked", false);
+    $("#asc").prop("checked", false);
     $("#numBin").prop("disabled", false);
     $("#numDec").prop("disabled", true);
     $("#numHex").prop("disabled", true);
     $("#numOct").prop("disabled", true);
-    $("#numDec").val('');
-    $("#numHex").val('');
-    $("#numOct").val('');
+    $("#numAsc").prop("disabled", true);
+    $("#numDec").val("");
+    $("#numHex").val("");
+    $("#numOct").val("");
+    $("#numAsc").val("");
   }
   if (event == "hex") {
     $("#hex").prop("checked", true);
     $("#dec").prop("checked", false);
     $("#bin").prop("checked", false);
     $("#oct").prop("checked", false);
+    $("#asc").prop("checked", false);
     $("#numHex").prop("disabled", false);
     $("#numDec").prop("disabled", true);
     $("#numBin").prop("disabled", true);
     $("#numOct").prop("disabled", true);
-    $("#numDec").val('');
-    $("#numBin").val('');
-    $("#numOct").val('');
+    $("#numAsc").prop("disabled", true);
+    $("#numDec").val("");
+    $("#numBin").val("");
+    $("#numOct").val("");
+    $("#numAsc").val("");
   }
   if (event == "oct") {
     $("#oct").prop("checked", true);
     $("#dec").prop("checked", false);
     $("#bin").prop("checked", false);
     $("#hex").prop("checked", false);
+    $("#asc").prop("checked", false);
     $("#numOct").prop("disabled", false);
     $("#numDec").prop("disabled", true);
     $("#numBin").prop("disabled", true);
     $("#numHex").prop("disabled", true);
-    $("#numDec").val('');
-    $("#numBin").val('');
-    $("#numHex").val('');
+    $("#numAsc").prop("disabled", true);
+    $("#numDec").val("");
+    $("#numBin").val("");
+    $("#numHex").val("");
+    $("#numAsc").val("");
+  }
+  if (event == "asc") {
+    $("#asc").prop("checked", true);
+    $("#bin").prop("checked", false);
+    $("#hex").prop("checked", false);
+    $("#oct").prop("checked", false);
+    $("#dec").prop("checked", false);
+    $("#numAsc").prop("disabled", false);
+    $("#numDec").prop("disabled", true);
+    $("#numBin").prop("disabled", true);
+    $("#numHex").prop("disabled", true);
+    $("#numOct").prop("disabled", true);
+    $("#numDec").val("");
+    $("#numBin").val("");
+    $("#numHex").val("");
+    $("#numOct").val("");
   }
 });
 
 $(".input").on("keyup", function (event) {
-
   switch (event.target.id) {
     case "numDec":
       Dec = parseInt($("#numDec").val());
       Bin = parseInt(Dec).toString(2);
       Hex = decimalToHexString(Dec);
       Oct = parseInt(Dec).toString(8);
+      Asc = String.fromCharCode(Dec);
       break;
 
     case "numBin":
@@ -75,6 +102,7 @@ $(".input").on("keyup", function (event) {
       Dec = parseInt(Bin, 2);
       Hex = decimalToHexString(Dec).toString(16);
       Oct = parseInt(Bin.toString(8));
+      Asc = String.fromCharCode(Dec);
       break;
 
     case "numHex":
@@ -82,6 +110,7 @@ $(".input").on("keyup", function (event) {
       Bin = Dec.toString(2);
       Hex = $("#numHex").val();
       Oct = Dec.toString(8);
+      Asc = String.fromCharCode(Dec);
       break;
 
     case "numOct":
@@ -89,33 +118,40 @@ $(".input").on("keyup", function (event) {
       Dec = parseInt(Oct, 8);
       Bin = Dec.toString(2);
       Hex = decimalToHexString(Dec);
+      Asc = String.fromCharCode(Dec);
       break;
-
+    case "numAsc":
+      Asc =  $("#numAsc").val();
+      Dec =   Asc.charCodeAt(0);
+      Bin =   Dec.toString(2);
+      Hex = decimalToHexString(Dec);
+      Oct = parseInt(Dec).toString(8);
+      break;
     default:
-        Dec= '';
-        Bin= '';
-        Hex= '';
-        Oct= '';
+      Dec = "";
+      Bin = "";
+      Hex = "";
+      Oct = "";
+      Asc = "";
       break;
   }
-  function decimalToHexString(number)
-  {
-    if (number < 0)
-    {
-      number = 0xFFFFFFFF + number + 1;
-    }else if(isNaN(number) == true){
-      return '';
+  function decimalToHexString(number) {
+    if (number < 0) {
+      number = 0xffffffff + number + 1;
+    } else if (isNaN(number) == true) {
+      return "";
     }
-  
+
     return number.toString(16).toUpperCase();
   }
-$("#numDec").val(Dec)
-$("#numBin").val(Bin)
-$("#numHex").val(`${Hex}`)
-$("#numOct").val(Oct)
-
-
+  $("#numDec").val(Dec);
+  $("#numBin").val(Bin);
+  $("#numHex").val(`${Hex}`);
+  $("#numOct").val(Oct);
+  $("#numAsc").val(Asc);
 
 });
+
+
 
 
